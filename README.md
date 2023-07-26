74e9da672fc8ab6d88ef72c80cef5395ac7a3f33
Dziyana Tsetserava <dtsetserava@groupe-isia.com>
2023-07-26 13:30:16 +0200

    feat: use env vars for database connection in api

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # dts-kube
 
-## Docker registry link:
-[Docker Hub](https://hub.docker.com/repository/docker/dziyana/hello-world/general)
+To log in to package registry:
+
+```
+echo MY_PERSONAL_ACCESS_TOKEN | docker login ghcr.io -u USERNAME --password-stdin
+```
+
+To create docker images go to a corresponding folder and run one of the following commands:
+
+```
+docker build -t ghcr.io/do3-2023/dts-kube-api:v1.0 .
+docker build -t ghcr.io/do3-2023/dts-kube-front:v1.0 .
+```
--- api/src/database/database.providers.ts
+++ api/src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'test',
-        password: 'test',
-        database: 'test',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],

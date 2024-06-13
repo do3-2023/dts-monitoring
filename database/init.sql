e27dc051eb5be8dd61249b6519cdfa4d0d80e0fc
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2024-06-13 14:36:25 +0200

    feat: init database with initcontainers in the api pod

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
 -- create a table
-CREATE TABLE person (
+CREATE TABLE IF NOT EXISTS person (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   last_name TEXT NOT NULL,
   phone_number TEXT NOT NULL,

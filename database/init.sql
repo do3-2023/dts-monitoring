7a734ddf22d00a598aab3225de80c37855cf2e12
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2024-06-17 13:54:04 +0200

    feat: make location attribute nullable in the db

--- database/init.sql
+++ database/init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS person (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   last_name TEXT NOT NULL,
   phone_number TEXT NOT NULL,
-  location TEXT NOT NULL
+  location TEXT
 );
 
 -- add test data

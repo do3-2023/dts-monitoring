ed91bb1d5664ae096fb2219942696032ef039101
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2024-06-19 10:32:15 +0200

    feat: completely remove location from the db conf

--- database/init.sql
+++ database/init.sql
@@ -2,6 +2,5 @@
 CREATE TABLE IF NOT EXISTS person (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   last_name TEXT NOT NULL,
-  phone_number TEXT NOT NULL,
-  location TEXT
+  phone_number TEXT NOT NULL
 );
